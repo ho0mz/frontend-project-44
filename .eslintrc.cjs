23d6561cccc5d266703170140dcd64d1edec0a64
ho0mz <ho0mzgg@gmail.com>
2026-04-12 20:35:43 +0300

    Configure ESLint v8 and update dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'brace-style': ['error', '1tbs'],
+    'arrow-parens': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'eol-last': ['error', 'always'],
+    'no-console': 'off',
+  },
+};
