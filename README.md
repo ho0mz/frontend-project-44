0b814471811a409646b457ed0cb168cb25a9eef1
ho0mz <ho0mzgg@gmail.com>
2026-04-19 17:27:43 +0300

    docs: add asciinema demo for brain-even game

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ho0mz/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ho0mz/frontend-project-44/actions)
+
+## Демонстрация работы игры "Проверка на чётность"
+[![asciicast](https://asciinema.org/a/17Ph6ATB6hpy6nzQ.svg)](https://asciinema.org/a/17Ph6ATB6hpy6nzQ)
---
+++ src/games/even.js
@@ -0,0 +1,15 @@
+import { runGame } from '../index.js';
+
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
+
+const generateRound = () => {
+  const question = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export const runEvenGame = () => {
+  runGame(gameDescription, generateRound);
+};
