935b4aacddd538aedcdc4ac83289156e281d9a11
ho0mz <ho0mzgg@gmail.com>
2026-04-19 21:40:31 +0300

    fix: replace .eslintrc.cjs with eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/eol-last': ['error', 'always'],
+      'no-console': 'off',
+    },
+  },
+];
