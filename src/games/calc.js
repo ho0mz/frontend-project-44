9dd4db5ac00b237e87cfac381b50ee9879baea92
ho0mz <ho0mzgg@gmail.com>
2026-04-19 18:53:00 +0300

    liu

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,14 +4,14 @@ const gameDescription = 'What is the result of the expression?';
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    default:
-      throw new Error(`Unknown operator: ${operator}`);
+  case '+':
+    return num1 + num2;
+  case '-':
+    return num1 - num2;
+  case '*':
+    return num1 * num2;
+  default:
+    throw new Error(`Unknown operator: ${operator}`);
   }
 };
 
@@ -20,10 +20,10 @@ const generateRound = () => {
   const num1 = Math.floor(Math.random() * 20) + 1; // число от 1 до 20
   const num2 = Math.floor(Math.random() * 20) + 1;
   const operator = operators[Math.floor(Math.random() * operators.length)];
-  
+
   const question = `${num1} ${operator} ${num2}`;
   const correctAnswer = String(calculate(num1, num2, operator));
-  
+
   return [question, correctAnswer];
 };
 
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,23 +6,23 @@ const gameDescription = 'Find the greatest common divisor of given numbers.';
 const findGcd = (a, b) => {
   let first = Math.abs(a);
   let second = Math.abs(b);
-  
+
   while (second !== 0) {
     const temp = second;
     second = first % second;
     first = temp;
   }
-  
+
   return first;
 };
 
 const generateRound = () => {
   const num1 = Math.floor(Math.random() * 50) + 1; // число от 1 до 50
   const num2 = Math.floor(Math.random() * 50) + 1;
-  
+
   const question = `${num1} ${num2}`;
   const correctAnswer = String(findGcd(num1, num2));
-  
+
   return [question, correctAnswer];
 };
 
